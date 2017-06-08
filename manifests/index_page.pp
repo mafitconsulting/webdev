7cd08b70b7efcc50fcb775a9375f6fe3878d637b
root <root@fieldhousem2.mylabserver.com>
2017-06-08 06:38:44 +0000

    updated profile

--- manifests/index_page.pp
+++ manifests/index_page.pp
@@ -1,7 +1,10 @@
-define webdev::index_page {
+define webdev::index_page 
+  String $content,
+) {
   file { 'create_index_file':
-    ensure => file,
-    group  => 'root',
-    mode   => '0644',
+    ensure  => file,
+    group   => 'root',
+    mode    => '0644',
+    content => $content,
   }
 }
